eca896f7bca0d7d61fcd60079fe80cefc9867767
Sri Abhishek Mamidi <mamidisriabhishek@gmail.com>
2024-03-07 13:34:32 +0530

    js file for the game

--- score.js
+++ score.js
@@ -1,1 +1,15 @@
 // Iteration 8: Making scoreboard functional
+/*var urlParams = new URLSearchParams(window.location.search);
+console.log(urlParams);
+var score = urlParams.get("score");*/
+let score = localStorage.getItem("score");
+console.log("Score = "+score);
+
+var scoreBoard = document.getElementById("score-board");
+
+var playAgainButton = document.getElementById("play-again-button");
+
+scoreBoard.innerHTML = score;
+playAgainButton.onclick = (evt) =>{
+    location.href = "./game.html";
+}
